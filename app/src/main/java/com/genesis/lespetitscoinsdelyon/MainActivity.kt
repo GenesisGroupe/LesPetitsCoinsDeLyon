8e9c858526d5091744092d22fb3c73e031efe4e7
Hugo PATURAL <hpatural69@gmail.com>
2018-02-22 10:10:45 +0100

    init project

---
+++ app/src/main/java/com/genesis/lespetitscoinsdelyon/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.genesis.lespetitscoinsdelyon
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by hpatural on 22/02/2018.
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.main_activity)
+    }
+}
